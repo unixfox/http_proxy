fdc2f0d37d587a16c1522bd57c7f7053975385ad
Emilien Devos <contact@emiliendevos.be>
2020-02-29 14:35:38 +0100

    use host for bind_tcp

--- samples/server.cr
+++ samples/server.cr
@@ -5,7 +5,7 @@ port = 8080
 
 server = HTTP::Proxy::Server.new(host, port)
 
-server.bind_tcp(port)
+server.bind_tcp(host, port)
 
 puts "Listening on http://#{server.host}:#{server.port}"
 server.listen
